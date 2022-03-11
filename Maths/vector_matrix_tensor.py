a32e5f962303aee65b957aa896fcc996f9beab8d
Talha Nebi Kumru <92807327+tkumru@users.noreply.github.com>
2022-03-11 16:06:54 +0300

    Vector, Matrix, Tensor
    Introduction to vector, matrix and tensor

---
+++ Maths/vector_matrix_tensor.py
@@ -0,0 +1,29 @@
+import numpy as np
+from keras.datasets import mnist
+
+# VECTOR ----------------------------
+
+x = np.array([1, 2, 3])
+print("Vector")
+print(f"Vector array: \n{x}")
+print(f"Vector dimension: {x.ndim}")
+print()
+# MATRIX ---------------------------
+print()
+x = np.array([[1,2,3],[2,3,4], [3,4,5]])
+print("Matrix")
+print(f"Matrix array: \n{x}")
+print(f"Matrix dimension: {x.ndim}")
+print()
+# TENSOR --------------------------------
+print()
+x = np.array([[[1,2,3],[2,3,4]], [[3,4,5], [4,5,6]], [[5,6,7], [6,7,8]]])
+print("Tensor")
+print(f"Tensor array: \n{x}")
+print(f"Tensor dimension: {x.ndim}")
+print()
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+print(f"Images dimension: {train_images.ndim}")
+print(f"Images shape (numbers, width, length): {train_images.shape}")
